f6ffbd867cc96e751f2a90236bbd8f5138129898
alexm323 <alejandromartinezjr42@gmail.com>
2021-05-14 21:46:55 -0700

    react task tracker app

---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+const Tasks = ({tasks,onDelete,onToggle}) => {
+    
+    return (
+        <>
+            {tasks.map((task) => (
+                <Task onDelete={onDelete} onToggle={onToggle} key={task.id} task={task} text={task.text}></Task>
+            ))}
+        </>
+    )
+}
+
+export default Tasks
